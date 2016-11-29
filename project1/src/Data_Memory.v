6d5415e0d0ad439e95d0a6b329a7b04cc5c75b15
peskotivesgeroff <b40815@gmail.com>
2016-11-29 15:01:38 +0800

    data mem; has bug

--- project1/src/Data_Memory.v
+++ project1/src/Data_Memory.v
@@ -1,10 +1,10 @@
 module Data_Memory
 (
-    addr_i,
-	data_i,
-	MemRead_i,
-	MemWrite_i,
-    data_o
+  addr_i,
+  data_i,
+  MemRead_i,
+  MemWrite_i,
+  data_o
 );
 
 // Interface
@@ -16,14 +16,18 @@ output  [31:0]      data_o;
 reg     [7:0]     	memory  [0:31]; //why 8 bits?
 reg 	[31:0]		data_o;
 
-always@(MemRead_i or MemWrite_i) begin
-    if(MemRead_i)
-        data_o = {{24'b0}, memory[addr_i>>2]};
-	else begin
-		data_o = 32'b0;
-		if(MemWrite_i)
-			memory[addr_i>>2] = data_i[7:0];
-	end
+always@(*) begin
+  if(MemRead_i) begin
+    data_o = {memory[addr_i+3], memory[addr_i+2], memory[addr_i+1], memory[addr_i]};
+    //data_o = {{24'b0}, memory[addr_i>>2]};
+  end else begin
+    data_o = 32'b0;
+    if(MemWrite_i)
+      memory[addr_i] = data_i[7:0];
+      memory[addr_i+1] = data_i[15:-8];
+      memory[addr_i+2] = data_i[23:-8];
+      memory[addr_i+3] = data_i[31:-8];
+  end
 end
 
 endmodule
